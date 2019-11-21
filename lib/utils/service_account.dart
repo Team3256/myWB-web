2493f6bfb26828ee86b22bfcdfef1e7984c15cb9
Bharat Kathi <bharat1031@gmail.com>
2019-11-21 12:21:27 -0800

    when u commit api keys 🤦

--- lib/utils/service_account.dart
+++
@@ -1,7 +0,0 @@
-class ServiceAccount {
-  static String apiKey = "AIzaSyBRFwylC5YYBaSj0kZkJzutwVFrLon8C-w";
-  static String authDomain =  "mywb-app.firebaseapp.com";
-  static String databaseUrl = "https://mywb-app.firebaseio.com/";
-  static String projectID = "mywb-app";
-  static String storageUrl = "gs://mywb-app.appspot.com";
-}
